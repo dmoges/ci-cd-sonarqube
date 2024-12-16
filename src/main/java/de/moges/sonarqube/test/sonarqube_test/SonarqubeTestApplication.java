2873b3498e983f0a5e08406148315e0fca371c36
Dawit Moges <dmogesde@gmx.net>
2024-12-16 14:09:42 +0100

    first commit

---
+++ src/main/java/de/moges/sonarqube/test/sonarqube_test/SonarqubeTestApplication.java
@@ -0,0 +1,13 @@
+package de.moges.sonarqube.test.sonarqube_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarqubeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarqubeTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/moges/sonarqube/test/sonarqube_test/SonarqubeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package de.moges.sonarqube.test.sonarqube_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarqubeTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
